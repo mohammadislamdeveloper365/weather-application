a28000ea8eb96110260b9d8880fb14fad743fbb6
Mohammad Moinul Islam <95491483+mohammadislamdeveloper365@users.noreply.github.com>
2023-11-25 20:03:51 +0400

    feat: add style to the weather details

--- javascript/index.js
+++ javascript/index.js
@@ -14,24 +14,34 @@ function loadWeather(cityName) {
     
     fetch(url)
     .then(response => response.json())
-    .then(data => displayWeatherDetails(data));
+    .then(data => displayWeatherDetails(data))
+    .catch(error=>console.log(error.message));
 }
 
 function displayWeatherDetails(weatherDetails) {
     const parentContainer = getElementById('weather-container');
-    const weatherDetailsContainer = createElement('div');
+    let weatherDetailsContainer = getElementById('weather-details-container');
+    if(weatherDetailsContainer) {
+        weatherDetailsContainer.innerHTML = '';
+    } else {
+        weatherDetailsContainer = createElement('div');
+        weatherDetailsContainer.id = 'weather-details-container';
+    }
     const cityNameElement = createElement('p');
     cityNameElement.innerText = weatherDetails?.name ?? '';
     const weatherConditionElement = createElement('p');
     weatherConditionElement.innerText = weatherDetails?.weather?.[0]?.description ?? ''
     const temperatureElement = createElement('p');
-    temperatureElement.innerText = weatherDetails?.main?.temp ?? ''
+    temperatureElement.innerText = weatherDetails?.main?.temp ?? '';
+    if(temperatureElement.innerText) {
+        temperatureElement.innerText = temperatureElement.innerText + " Celcius"
+    }
     weatherDetailsContainer.appendChild(cityNameElement);
     weatherDetailsContainer.appendChild(weatherConditionElement);
     weatherDetailsContainer.appendChild(temperatureElement);
     parentContainer.appendChild(weatherDetailsContainer);
-
+    cityNameElement.classList.add('weather-details');
+    temperatureElement.classList.add('weather-details');
+    weatherConditionElement.classList.add('weather-details');
     console.log(weatherDetails)
-    
-    
 }
